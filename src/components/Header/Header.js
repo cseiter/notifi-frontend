03b652e07d838fced06c1b5246148af38822473b
chris <cseiter@gmail.com>
2021-10-01 19:20:02 -0400

    add in basic div laters for header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import basic_app_logo from './basic_app_logo.png';
 
 function Header() {
 
     return (
         <main>
-            Notifi Ticketing System
+            <div className="logo">
+                <img src={basic_app_logo} width="100" height="40" alt="Notifi logo"/>
+            </div>
+            <div className="companyName">
+                Notifi Ticketing System
+            </div>
         </main>
     );
 }
