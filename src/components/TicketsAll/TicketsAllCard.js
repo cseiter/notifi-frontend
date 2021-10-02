ede6e5cd1282fea45bb21460aef76abd03b6876d
chris <cseiter@gmail.com>
2021-10-02 10:50:34 -0400

    fix typo from copying from previous functional project

--- src/components/TicketsAll/TicketsAllCard.js
+++ src/components/TicketsAll/TicketsAllCard.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './TicketsAll.css';
 
-function TicketsAllCard({indCategory}) {
+function TicketsAllCard({indTicketsAll}) {
     const {id,ticket_title,ticket_number, created_at,updated_at,users_id,devices_id,stations_id,status_id} = indTicketsAll;
-}
 
     return (
         <div className="ticketRow">
             {id}{ticket_title}{ticket_number}{created_at}{updated_at}{users_id}{devices_id}{stations_id}{status_id}
         </div>
         );
+
+    }
 
 export default TicketsAllCard;
