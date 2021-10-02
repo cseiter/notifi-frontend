5b723ccb397e2c6e46e7e26b8cbfcc707c427b5e
chris <cseiter@gmail.com>
2021-10-01 20:31:48 -0400

    create initial card for ticket overview

---
+++ src/components/TicketsAll/TicketsAllCard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './TicketsAll.css';
+
+function TicketsAllCard({indCategory}) {
+    const {id,ticket_title,ticket_number, created_at,updated_at,users_id,devices_id,stations_id,status_id} = indTicketsAll;
+}
+
+    return (
+        <div className="ticketRow">
+            {id}{ticket_title}{ticket_number}{created_at}{updated_at}{users_id}{devices_id}{stations_id}{status_id}
+        </div>
+        );
+
+export default TicketsAllCard;
