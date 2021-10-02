d2659350a1f6646b047a1fbc8ffc05d6b7f94ba5
chris <cseiter@gmail.com>
2021-10-02 11:30:36 -0400

    minor edits to try to fix map error

--- src/components/TicketsAll/TicketsAllCard.js
+++ src/components/TicketsAll/TicketsAllCard.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import './TicketsAll.css';
 
-function TicketsAllCard({indTicketsAll}) {
-    const {id,ticket_title,ticket_number, created_at,updated_at,users_id,devices_id,stations_id,status_id} = indTicketsAll;
+function TicketsAllCard({ticket}) {
+    const {id,ticket_title,ticket_number, created_at,updated_at,users_id,devices_id,stations_id,status_id} = ticket;
+    console.log(ticket);
 
     return (
         <div className="ticketRow">
-            {id}{ticket_title}{ticket_number}{created_at}{updated_at}{users_id}{devices_id}{stations_id}{status_id}
+            {id}
+            {ticket_title}
+            {ticket_number}
+            {created_at}
+            {updated_at}
+            {users_id}
+            {devices_id}
+            {stations_id}
+            {status_id}
         </div>
         );
 
--- src/components/TicketsAll/TicketsAllContainer.js
+++ src/components/TicketsAll/TicketsAllContainer.js
@@ -2,24 +2,17 @@ import React from 'react';
 import TicketsAllCard from './TicketsAllCard';
 
 function TicketsAllContainer({tickets}) {
-    const TicketsAllCards = tickets.map((ticket) => (
+    const ticketsAllCards = tickets.map((ticket) => (
         <TicketsAllCard
             key={ticket.id}
-            indTicketTitle={ticket.ticket_title}
-            indTicketNumber={ticket.ticket_number}
-            indTicketCreate={ticket.created_at}
-            indTicketUpdate={ticket.updated_at}
-            indTicketOwner={ticket.users_id}
-            indTicketDevice={ticket.devices_id}
-            indTicketStation={ticket.stations_id}
-            indTicketStatus={ticket.status_id}
+            ticket_overview={ticket}
         />
     ));
 
     return (
         <section>
             <h2>Current Tickets</h2>
-            {TicketsAllCards}
+            {ticketsAllCards}
         </section>
     );
 }
