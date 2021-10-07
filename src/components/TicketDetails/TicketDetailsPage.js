656bd742e78b651562a5e11838e9202a9c37ecfd
chris <cseiter@gmail.com>
2021-10-07 11:16:15 -0400

    fix ticket details not showing up

--- src/components/TicketDetails/TicketDetailsPage.js
+++ src/components/TicketDetails/TicketDetailsPage.js
@@ -1,22 +1,22 @@
 import React, {useEffect, useState} from 'react';
-import TicketDetailsCard from './TicketDetailsCard';
+import TicketDetailsContainer from './TicketDetailsContainer';
 
 function TicketDetailsPage() {
-    const [tickets,setTickets] = useState([]);
-  
-    useEffect(() => {
-      fetch('https://ancient-retreat-67722.herokuapp.com/tickets/1')
-      .then((r) => r.json())
-      .then(setTickets);
-    },[]);
-  
-    return (
-      <main>
-        <TicketDetailsCard
-        ticket={tickets}
-        />
-      </main>
-  );
-  }
-  
-  export default TicketDetailsPage;
+  const [tickets,setTickets] = useState([]);
+
+  useEffect(() => {
+    fetch('https://ancient-retreat-67722.herokuapp.com/tickets')
+    .then((r) => r.json())
+    .then(setTickets);
+  },[]);
+
+  return (
+    <main>
+      <TicketDetailsContainer
+      tickets={tickets}
+      />
+    </main>
+);
+}
+
+export default TicketDetailsPage;
