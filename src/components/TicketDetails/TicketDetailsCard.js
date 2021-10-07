1de691a543cfa2befac5f128855bbda3b3ef4f61
chris <cseiter@gmail.com>
2021-10-07 13:01:23 -0400

    continuing style changes

--- src/components/TicketDetails/TicketDetailsCard.js
+++ src/components/TicketDetails/TicketDetailsCard.js
@@ -11,16 +11,16 @@ function TicketDetailsCard({indTicket}) {
                 <td className="title">{ticket_title}</td>
             </tr>
             <tr>
-                <td>Device Affected ID: {devices_id}<br />
+                <td className="section">Device Affected ID: {devices_id}<br />
                 Station Affected ID: {stations_id}</td>
                 <td rowspan="3" colspan="2">{ticket_details}</td>
             </tr>
             <tr>
-                <td>Created At: {created_at}< br/>
+                <td className="section">Created At: {created_at}< br/>
                 Last Updated At: {updated_at}</td>
             </tr>
             <tr>
-                <td>Ticket Owner ID: {users_id}<br />
+                <td className="section">Ticket Owner ID: {users_id}<br />
                 Ticket Status ID: {status_id}</td>
             </tr>
         </tbody>
