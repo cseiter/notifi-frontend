93f77875cc904092f33af0a5d2fabdcaed2ad6fb
chris <cseiter@gmail.com>
2021-10-12 12:57:18 -0400

    first attempt at ticket with comments page

---
+++ src/components/TicketWithComments/TicketWithCommentsPage.js
@@ -0,0 +1,28 @@
+import React, {useEffect, useState} from 'react';
+import TicketWithCommentsContainer from './TicketWithCommentsContainer';
+
+function TicketWithCommentsPage() {
+  const [comments,setComments] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/comments/1')
+    .then((r) => r.json())
+    .then(setComments);
+  },[]);
+
+  function handleDeleteComment(commentToDelete) {
+    const updatedComments = comments.filter((comment) => comment.id !== commentToDelete.id);
+    setComments(updatedComments);
+  }
+
+  return (
+    <main>
+      <TicketWithCommentsContainer
+      comments={comments}
+      onDeleteComments={handleDeleteComment}
+      />
+    </main>
+);
+}
+
+export default TicketWithCommentsPage;
