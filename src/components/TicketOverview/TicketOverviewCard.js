81b9b55782a2fb39259deca03ff580b76a572dbc
chris <cseiter@gmail.com>
2021-10-13 18:48:02 -0400

    deleted updated to hosted site and tested successfully

--- src/components/TicketOverview/TicketOverviewCard.js
+++ src/components/TicketOverview/TicketOverviewCard.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import {BrowserRouter as Router, Link} from 'react-router-dom';
 import './TicketOverview.css';
 
 function TicketOverviewCard({indTicket,onDeleteTicket}) {
     const {id,ticket_title,created_at,updated_at,users_id,devices_id,stations_id,statuses_id} = indTicket;
 
     function handleDeleteClick() {
-        fetch(`http://localhost:3000/tickets/${id}`, {
+        fetch(`https://desolate-wave-23214.herokuapp.com/tickets/${id}`, {
             method: "DELETE",
     })
     .then(() => {onDeleteTicket(indTicket);
@@ -16,7 +15,7 @@ function TicketOverviewCard({indTicket,onDeleteTicket}) {
     return (
         <tbody>
             <tr className="test">
-                <td><Router><Link to={{ pathname: `http://localhost:3000/tickets/${id}`}} target="_blank" rel="noopener noreferrer">{id}</Link></Router></td>
+                <td>{id}</td>
                 <td>{ticket_title}</td>
                 <td>{created_at}</td>
                 <td>{updated_at}</td>
